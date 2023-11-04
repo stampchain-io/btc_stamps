3b1867d89052bedf400d43a3d31c2f1c24309d34
javiercervilla <javier@cervilla.es>
2023-11-04 19:50:49 +0100

    feat(SQLITE_REMOVE):[WIP] Working on it, there is an error with the connection between parsing blocks

--- start.py
+++ start.py
@@ -1,12 +1,7 @@
-
-
-
-
 import server
 
-database_file = 'server.db'
 log_file = 'log.file'
-db = server.initialise(database_file=database_file, log_file=log_file, backend_password='rpc', backend_user='rpc' )
+db = server.initialise( log_file=log_file, backend_password='rpc', backend_user='rpc' )
 
 if db is None:
     print('Failed to connect to database')
