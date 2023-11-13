c982ba383c71829ff4ed3788586d99b42a54b30f
javiercervilla <javier@cervilla.es>
2023-11-13 20:16:55 +0100

    feat(Test):[DEV] check issuance with api

--- xcprequest.py
+++ xcprequest.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 import requests
 from requests.auth import HTTPBasicAuth
 
-url = config.CP_RPC_URL + "/api" # "http://public.coindaddy.io:4000/api/"
+url = config.CP_RPC_URL + "/api/rest" # "http://public.coindaddy.io:4000/api/"
 auth = HTTPBasicAuth(config.CP_RPC_USER, config.CP_RPC_PASSWORD)
 
 base_payload = {
@@ -16,7 +16,7 @@ base_payload = {
   "id": 0
 }
 
-def creat_payload(method, params):
+def create_payload(method, params):
     base_payload["method"] = method
     base_payload["params"] = params
     return base_payload
@@ -24,7 +24,8 @@ def creat_payload(method, params):
 # counterparty public node: https://public.coindaddy.io:4000/api/b
 
 def get_issuances_by_block(block_index):
-    payload = creat_payload("get_issuances", {"filters": { "field": "block_index", "op" : "==", "value" : block_index }})
+    payload = create_payload("get_issuances", {"filters": { "field": "block_index", "op" : "==", "value" : block_index }})
+    logger.warning("Payload: {}".format( payload))
     headers = {'content-type': 'application/json'}
     response = requests.post(url, data=json.dumps(payload), headers=headers, auth=auth)
     logger.warning("Response: {}".format( response))
