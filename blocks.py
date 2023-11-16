27a2fe4fd0c3421686966d215c9820340137f4f7
javiercervilla <javier@cervilla.es>
2023-11-16 18:46:18 +0100

    feat(check_src_data):[+] DEBUG

--- blocks.py
+++ blocks.py
@@ -673,7 +673,7 @@ def list_tx(db, block_hash, block_index, block_time, tx_hash, tx_index, tx_hex=N
             source = str(stamp_issuance['source'])
             destination = str(stamp_issuance['issuer'])
         key_burn = 0
-        if decoded_tx is not None:
+        if decoded_tx is not None and 'vout' in decoded_tx:
             key_burn = check_burnkeys_in_multisig(decoded_tx)
         # logger.warning('Saving to MySQL transactions: {}\nDATA:{}'.format(tx_hash, data))
         cursor.execute(
