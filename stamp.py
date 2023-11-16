0d25815aaf6b210598be2c72b4cb01ce94210e8a
javiercervilla <javier@cervilla.es>
2023-11-17 00:58:27 +0100

    fix(src721):[DEBUG] remove src_data decode to utf-8

--- stamp.py
+++ stamp.py
@@ -372,7 +372,7 @@ def parse_stamps_to_stamp_table(db, stamps):
                 ).strftime('%Y-%m-%d %H:%M:%S'),
                 "tx_hash": tx_hash,
                 "tx_index": tx_index,
-                "src_data": json.dumps(src_data),
+                "src_data": src_data,
                 "stamp_gen": None,  # TODO: add stamp_gen,
                 "stamp_hash": stamp_hash,
             }
