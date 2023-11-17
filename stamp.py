3b80a4bab4a192d51ab6ac242524676505b7b57f
javiercervilla <javier@cervilla.es>
2023-11-17 03:56:08 +0100

    feat(btc_stamp):[+] Add check for btc_stamp

--- stamp.py
+++ stamp.py
@@ -243,6 +243,7 @@ def check_burnkeys_in_multisig(transaction):
                 for item in asm:
                     if isinstance(item, bytes):
                         logger.warning(f"item: {item.hex()}")
+                        logger.warning(f"burnkey: {burnkey}")
                         if item.hex() == burnkey:
                             return 1
     return None
