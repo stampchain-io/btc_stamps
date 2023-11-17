d7780f6ca4463165d72f0f19da7efe8321e3bdff
javiercervilla <javier@cervilla.es>
2023-11-17 03:59:21 +0100

    feat(btc_stamp):[+] Add check for btc_stamp

--- blocks.py
+++ blocks.py
@@ -660,7 +660,7 @@ def list_tx(db, block_hash, block_index, block_time, tx_hash, tx_index, tx_hex=N
 
     source, destination, btc_amount, fee, data, decoded_tx, keyburn = get_tx_info(tx_hex, db=db) # type: ignore
 
-    logger.warning("keyburn after get_tx_info: {}".format(keyburn))
+    #logger.warning("keyburn after get_tx_info: {}".format(keyburn))
     # For mempool
     if block_hash is None:
         block_hash = config.MEMPOOL_BLOCK_HASH
@@ -674,7 +674,7 @@ def list_tx(db, block_hash, block_index, block_time, tx_hash, tx_index, tx_hex=N
             data = str(stamp_issuance)
             source = str(stamp_issuance['source'])
             destination = str(stamp_issuance['issuer'])
-        # logger.warning('Saving to MySQL transactions: {}\nDATA:{}'.format(tx_hash, data))
+        logger.warning('Saving to MySQL transactions: {}\nDATA:{}\n{}'.format(tx_hash, data, keyburn))
         cursor.execute(
             '''INSERT INTO transactions (
                 tx_index,
