fc7d1ed1dff58680bfd4bbd96c4242ed5ea81c35
reinamora <reinamora@stampchain.io>
2023-11-23 21:34:49 -0600

    feat: add .env var imort

--- start.py
+++ start.py
@@ -1,4 +1,10 @@
 import server
+import os
+
+with open('.env') as f:
+    for line in f:
+        key, value = line.strip().split('=')
+        os.environ[key] = value
 
 log_file = 'log.file'
 db = server.initialize( log_file=log_file, backend_password='rpc', backend_user='rpc' )
