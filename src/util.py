3845da46d779da18108df1e1b0a2db160d45334b
javiercervilla <javier@cervilla.es>
2023-11-25 20:40:23 +0100

    feat(CP_BLOCK_COUNT):[+] Add cp_block_count to util to dont fetch too much to quicknode

--- src/util.py
+++ src/util.py
@@ -28,7 +28,7 @@ import config
 
 D = decimal.Decimal
 B26_DIGITS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
+CP_BLOCK_COUNT = None
 # subasset contain only characters a-zA-Z0-9.-_@!
 SUBASSET_DIGITS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.-_@!'
 SUBASSET_REVERSE = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,
