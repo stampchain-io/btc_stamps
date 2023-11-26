e6a9111f5f4cb8237ec169157dbbcefbb802a2bf
javiercervilla <javier@cervilla.es>
2023-11-27 00:17:31 +0100

    feat(fresh):[+] Trying fresh

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -2,6 +2,8 @@
 
 echo "Creating database: $MYSQL_DATABASE with user $MYSQL_USER with all permisions"
 echo "Creating database: $MYSQL_DATABASE with user $GRAFANA_USER with select permision"
+echo "Creating database: $MYSQL_DATABASE with user $APP_USER with select permision"
+
 
 cat << EOF > /docker-entrypoint-initdb.d/init-db.sql
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
@@ -9,6 +11,8 @@ CREATE USER '$MYSQL_USER'@'%' IDENTIFIED WITH mysql_native_password BY '$MYSQL_P
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 CREATE USER '$GRAFANA_USER'@'%' IDENTIFIED WITH mysql_native_password BY '$GRAFANA_PASSWORD';
 GRANT SELECT ON $MYSQL_DATABASE.* TO '$GRAFANA_USER'@'%';
+CREATE USER '$APP_USER'@'%' IDENTIFIED WITH mysql_native_password BY '$APP_PASSWORD';
+GRANT SELECT ON $MYSQL_DATABASE.* TO '$APP_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
