56d19192befd51e0cb91e313737e9b09b739a5df
reinamora <kevin@moonblink.com>
2023-11-27 12:39:38 -0600

    debug: validation for stamps that need to be included

--- indexer/whitelist.py
+++ indexer/whitelist.py
@@ -2,11 +2,24 @@ import json
 
 
 def is_tx_in_whitelist(tx_hash):
-    whitelist_path = 'indexer/whitelist.json'
+    whitelist_path = 'whitelist.json'
     try:
         with open(whitelist_path, 'r') as file:
             whitelist = json.load(file)
         return tx_hash in whitelist.get('op_return', [])
+    except FileNotFoundError:
+        print(f"The file {whitelist_path} was not found.")
+        return False
+    except json.JSONDecodeError:
+        print(f"The file {whitelist_path} is not a valid JSON file.")
+        return False
+
+def is_to_include(tx_hash):
+    whitelist_path = 'whitelist.json'
+    try:
+        with open(whitelist_path, 'r') as file:
+            whitelist = json.load(file)
+        return tx_hash in whitelist.get('to_include', [])
     except FileNotFoundError:
         print(f"The file {whitelist_path} was not found.")
         return False
