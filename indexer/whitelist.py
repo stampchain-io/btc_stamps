e73c839f211bc83c04ee18fe28ae4736f4f3001d
reinamora <kevin@moonblink.com>
2023-11-27 17:07:54 -0600

    fix: remove stamp_gen column - no longer needed, update WL

--- indexer/whitelist.py
+++ indexer/whitelist.py
@@ -1,5 +1,6 @@
 import json
 
+# DEBUG ONLY 
 
 def is_tx_in_whitelist(tx_hash):
     whitelist_path = 'whitelist.json'
@@ -20,6 +21,19 @@ def is_to_include(tx_hash):
         with open(whitelist_path, 'r') as file:
             whitelist = json.load(file)
         return tx_hash in whitelist.get('to_include', [])
+    except FileNotFoundError:
+        print(f"The file {whitelist_path} was not found.")
+        return False
+    except json.JSONDecodeError:
+        print(f"The file {whitelist_path} is not a valid JSON file.")
+        return False
+
+def is_to_exclude(tx_hash):
+    whitelist_path = 'whitelist.json'
+    try:
+        with open(whitelist_path, 'r') as file:
+            whitelist = json.load(file)
+        return tx_hash in whitelist.get('invalid_src20_no_keyburn', [])
     except FileNotFoundError:
         print(f"The file {whitelist_path} was not found.")
         return False
