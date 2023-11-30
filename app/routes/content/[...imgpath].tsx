ccbac168f8ccd20fb495b45977ada45ec6d5258d
javiercervilla <javier@cervilla.es>
2023-11-30 13:35:47 +0100

    fix(static):[+] Try to fix static files to be updated

--- app/routes/content/[...imgpath].tsx
+++ app/routes/content/[...imgpath].tsx
@@ -1,8 +1,20 @@
 import { HandlerContext } from "$fresh/server.ts";
 
-export function handler(req: Request, ctx: HandlerContext): Response {
-  return new Response("", {
-    status: 307,
-    headers: { Location: `/stamps/${ctx.params.imgpath}` },
-  });
+export async function handler(
+  req: Request,
+  ctx: HandlerContext,
+): Promise<Response> {
+  const path = `../../static/stamp/${ctx.params.imgpath}`;
+  try {
+    const file = await Deno.readFile(path);
+    return new Response(file, {
+      status: 200,
+      headers: {
+        "Content-Type": "image/jpeg", // Asegúrate de ajustar el tipo de contenido según el tipo de archivo de imagen
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response("File not found", { status: 404 });
+  }
 }
