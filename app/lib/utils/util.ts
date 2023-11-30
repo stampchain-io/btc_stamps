5ee73dfc01717d28766c2c6f30c0850d7065388a
javiercervilla <javier@cervilla.es>
2023-11-30 13:45:11 +0100

    fix(static):[+] Try to fix static files to be updated

--- app/lib/utils/util.ts
+++ app/lib/utils/util.ts
@@ -1,3 +1,33 @@
+const mimeTypes: { [key: string]: string } = {
+  "jpg": "image/jpeg",
+  "jpeg": "image/jpeg",
+  "png": "image/png",
+  "gif": "image/gif",
+  "svg": "image/svg+xml",
+  "tif": "image/tiff",
+  "jfif": "image/jpeg",
+  "jpe": "image/jpeg",
+  "pbm": "image/x-portable-bitmap",
+  "pgm": "image/x-portable-graymap",
+  "ppm": "image/x-portable-pixmap",
+  "pnm": "image/x-portable-anymap",
+  "apng": "image/apng",
+  "bmp": "image/bmp",
+  "webp": "image/webp",
+  "heif": "image/heif",
+  "heic": "image/heic",
+  "avif": "image/avif",
+  "ico": "image/x-icon",
+  "tiff": "image/tiff",
+  "svgz": "image/svg+xml",
+  "wmf": "image/wmf",
+  "emf": "image/emf",
+  "pcx": "image/pcx",
+  "djvu": "image/vnd.djvu",
+  "djv": "image/vnd.djvu",
+  "html": "text/html",
+};
+
 export const get_suffix_from_mimetype = (mimetype: string) => {
   const suffix = mimetype.split("/")[1];
   if (suffix === "jpeg") return "jpg";
@@ -7,4 +37,10 @@ export const get_suffix_from_mimetype = (mimetype: string) => {
 
 export const short_address = (address: string) => {
   return address.slice(0, 6) + "..." + address.slice(-6);
+};
+
+export const getMimeType = (extension: string): string => {
+  const normalizedExt = extension.toLowerCase();
+
+  return mimeTypes[normalizedExt] || "application/octet-stream";
 };
