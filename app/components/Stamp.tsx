ec4f1a9ec9b24debed489a94608fd7ae86a51165
javiercervilla <javier@cervilla.es>
2023-12-01 02:21:44 +0100

    feat(FBIMG):[+] testing fallback image and iframe

--- app/components/Stamp.tsx
+++ app/components/Stamp.tsx
@@ -2,48 +2,46 @@ import { get_suffix_from_mimetype } from "$lib/utils/util.ts";
 import { BASE_URL } from "$lib/utils/constants.ts";
 
 export const Stamp = ({ stamp }: { stamp: StampRow }) => {
+
+  if (stamp.stamp_mimetype === "text/html") {
+    return (
+      <iframe
+        class="w-16 h-16"
+        src={`/content/${stamp.tx_hash}.${get_suffix_from_mimetype(stamp.stamp_mimetype)
+          }`}
+        onError={(e) => {
+          e.currentTarget.src = stamp.stamp_url;
+        }}
+        alt="Stamp"
+      />
+    )
+  };
+  if (!stamp.stamp_mimetype) {
+    return (
+      <img
+        class="w-16 h-16"
+        style={{ imageRendering: "pixelated" }}
+        src={`/content/not-available.png`}
+        onError={(e) => {
+          console.log({ e });
+          e.currentTarget.src = `/content/not-available.png`;
+        }}
+        alt="Stamp"
+      />
+    )
+  };
   return (
-    stamp.stamp_mimetype === "text/html" 
-      ? (
-        <iframe
-          class="w-16 h-16"
-          style={{ imageRendering: "pixelated" }}
-          src={`/content/${stamp.tx_hash}.${
-            get_suffix_from_mimetype(stamp.stamp_mimetype)
-          }`}
-          onError={(e) => {
-            e.currentTarget.src = stamp.stamp_url;
-          }}
-          alt="Stamp"
-        />
-      )
-      :
-      !stamp.stamp_mimetype ? (
-        <img
-          class="w-16 h-16"
-          style={{ imageRendering: "pixelated" }}
-          src={`/content/not-available.png`}
-          onError={(e) => {
-            console.log({e});
-            e.currentTarget.src = `/content/not-available.png`;
-          }}
-          alt="Stamp"
-        />
-      ) :
-      (
-        <img
-          class="w-16 h-16"
-          style={{ imageRendering: "pixelated" }}
-          src={`${BASE_URL}/content/${stamp.tx_hash}.${
-            get_suffix_from_mimetype(stamp.stamp_mimetype)
-          }`}
-          onError={(e) => {
-            console.log({e});
-            e.currentTarget.src = `/content/not-available.png`;
-          }}
-          alt="Stamp"
-        />
-      )
-  );
+    <img
+      class="w-16 h-16"
+      style={{ imageRendering: "pixelated" }}
+      src={`${BASE_URL}/content/${stamp.tx_hash}.${get_suffix_from_mimetype(stamp.stamp_mimetype)
+        }`}
+      onError={(e) => {
+        console.log({ e });
+        e.currentTarget.src = `/content/not-available.png`;
+      }}
+      alt="Stamp"
+    />
+  )
 };
 export default Stamp;
