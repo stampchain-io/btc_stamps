f54f9c6fb5867af7bc933206d627624bf2dadbc8
moonblink <reinamora@stampchain.io>
2023-12-19 04:42:21 +0000

    feat(src20): index on db for s3objects

--- indexer/table_schema.sql
+++ indexer/table_schema.sql
@@ -188,5 +188,6 @@ CREATE TABLE IF NOT EXISTS s3objects (
     `id` VARCHAR(255) NOT NULL,
     `path_key` VARCHAR(255) NOT NULL,
     `md5` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    index `path_key` (`path_key`)
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
