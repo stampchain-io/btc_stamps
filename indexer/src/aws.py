cea071e0d2667a8e8e7ae743fa39a63c3e71c6fb
moonblink <reinamora@stampchain.io>
2023-12-19 05:21:05 +0000

    feat(aws): aws s3objects database storage to inhibit re-upload of existing file

--- indexer/src/aws.py
+++ indexer/src/aws.py
@@ -9,27 +9,83 @@ import src.log as log
 logger = logging.getLogger(__name__)
 log.set_logger(logger)  # set root logger
 
-''' this file is intended for optional file upload to AWS S3 - WIP - NOT IMPLEMENTED'''
+''' this is intended for optional file upload to AWS S3 and Cloudfront CDN.'''
 
 
-def get_s3_objects(bucket_name, s3_client):
+def get_s3_objects(db, bucket_name, s3_client):
     ''' this gets existing objects in S3 so we don't reupload existing files which can add to AWS costs'''
-    # TODO: Save this in a new table for future reference and to avoid having to fetch this every time. 
-    # will need to update the table ofc if a file is over-written or if we re-index with a new bg image
-    result = []
-    paginator = s3_client.get_paginator('list_objects_v2')
-    logger.info(f"Fetching S3 objects from bucket: {bucket_name}/{config.AWS_S3_IMAGE_DIR}...")
-    pages = list(paginator.paginate(Bucket=bucket_name, Prefix=config.AWS_S3_IMAGE_DIR))
-    total_pages = len(pages)
-    current_page = 0
+    cursor = db.cursor()
+    cursor.execute("SELECT path_key, md5 FROM s3objects")
+    results = cursor.fetchall() or []
+    results = [{'key': row[0], 'md5': row[1]} for row in results]
+    cursor.close()
+    if results:
+        logger.info(f"Found {len(results)} existing S3 objects")
+    else:
+        paginator = s3_client.get_paginator('list_objects_v2')
+        logger.info(f"Fetching S3 objects from bucket: {bucket_name}/{config.AWS_S3_IMAGE_DIR}...")
+        pages = list(paginator.paginate(Bucket=bucket_name, Prefix=config.AWS_S3_IMAGE_DIR))
+        total_pages = len(pages)
+        current_page = 0
 
-    for current_page, page in enumerate(pages, start=1):
-        if 'Contents' in page:
-            for obj in tqdm(page['Contents'], desc=f'Fetching S3 objects (Page {current_page}/{total_pages})', unit=' object', bar_format='{desc}: {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt}'):
-                s3_object = s3_client.head_object(Bucket=bucket_name, Key=obj['Key'])
-                result.append({'key': obj['Key'], 'md5': s3_object['ETag'].strip('"')})
+        for current_page, page in enumerate(pages, start=1):
+            if 'Contents' in page:
+                for obj in tqdm(page['Contents'], desc=f'Fetching S3 objects (Page {current_page}/{total_pages})', unit=' object', bar_format='{desc}: {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt}'):
+                    s3_object = s3_client.head_object(Bucket=bucket_name, Key=obj['Key'])
+                    results.append({'key': obj['Key'], 'md5': s3_object['ETag'].strip('"')})
 
-    return result
+    if results:
+        logger.info(f"Found {len(results)} existing S3 objects")
+        add_s3_objects_to_db(db, results)
+    else:
+        logger.info(f"No existing S3 objects found")
+
+    return results
+
+
+def update_dbobjects(db, filename, file_obj_md5):
+    ''' this updates the s3objects db table with any new objects that have been uploaded to S3'''
+    try:
+        s3_file_path = f"{config.AWS_S3_IMAGE_DIR}{filename}"
+        id = f"{s3_file_path}_{file_obj_md5}"
+
+        cursor = db.cursor()
+        
+        # Check if the filename already exists in the table
+        cursor.execute("SELECT id FROM s3objects WHERE path_key = %s", (s3_file_path,))
+        existing_id = cursor.fetchone()
+        
+        if existing_id:
+            # Delete the existing row
+            cursor.execute("DELETE FROM s3objects WHERE id = %s", (existing_id[0],))
+        
+        # Insert the new object
+        cursor.execute("INSERT IGNORE INTO s3objects (id, path_key, md5) VALUES (%s, %s, %s)", (id, s3_file_path, file_obj_md5))
+        
+        cursor.close()
+    except Exception as e:
+        logger.warning(f"ERROR: Unable to update the s3objects table. Error: {e}")
+
+
+def add_s3_objects_to_db(db, s3_objects):
+    ''' Add S3 objects to the s3objects table'''
+    try:
+        cursor = db.cursor()
+
+        for s3_object in s3_objects:
+            path_key = s3_object['key']
+            md5 = s3_object['md5']
+            id = path_key + md5
+
+            query = "INSERT IGNORE INTO s3objects (id, path_key, md5) VALUES (%s, %s, %s)"
+            values = (id, path_key, md5)
+            cursor.execute(query, values)
+
+        cursor.close()
+
+        logger.info("S3 objects added to the database successfully")
+    except Exception as e:
+        logger.warning(f"ERROR: Unable to add S3 objects to the database. Error: {e}")
 
 
 def invalidate_s3_files(file_paths, aws_cloudfront_distribution_id):
@@ -59,7 +115,7 @@ def upload_file_to_s3(file_obj_or_path, bucket_name, s3_file_path, s3_client, co
         logger.warning(f"failure uploading to aws {e}")
 
 
-def check_existing_and_upload_to_s3(filename, mime_type, file_obj, file_obj_md5):
+def check_existing_and_upload_to_s3(db, filename, mime_type, file_obj, file_obj_md5):
     s3_file_path = f"{config.AWS_S3_IMAGE_DIR}{filename}"
     if mime_type is None:
         mime_type = 'binary/octet-stream'
@@ -73,6 +129,7 @@ def check_existing_and_upload_to_s3(filename, mime_type, file_obj, file_obj_md5)
                 file_obj.seek(0)
                 logger.debug(f"Uploading {filename} with changed hash {file_obj_md5} to S3...")
                 upload_file_to_s3(file_obj, config.AWS_S3_BUCKETNAME, s3_file_path, config.AWS_S3_CLIENT, content_type=mime_type)
+                update_dbobjects(db, filename, file_obj_md5)
                 if config.AWS_CLOUDFRONT_DISTRIBUTION_ID:
                     logger.debug(f"Invalidating {filename} with changed hash {file_obj_md5} in Cloudfront...")
                     invalidate_s3_files(["/" + s3_file_path], config.AWS_CLOUDFRONT_DISTRIBUTION_ID)
@@ -83,5 +140,7 @@ def check_existing_and_upload_to_s3(filename, mime_type, file_obj, file_obj_md5)
             file_obj.seek(0)
             logger.debug(f"Uploading new {filename} with hash {file_obj_md5} to S3...")
             upload_file_to_s3(file_obj, config.AWS_S3_BUCKETNAME, s3_file_path, config.AWS_S3_CLIENT, content_type=mime_type)
+            # need to delete old object from s3objects table
+            update_dbobjects(db, filename, file_obj_md5)
         except Exception as e:
             logger.warning(f"ERROR: Unable to upload {filename} to S3. Error: {e}")
