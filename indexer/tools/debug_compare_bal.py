eb16af68e444c76a42f845334a0ba5d9a177db90
Reinamora <reinamora@stampchain.io>
2023-12-28 19:27:35 +0000

    chore(src20): additional balance table comparisons

--- indexer/tools/debug_compare_bal.py
+++ indexer/tools/debug_compare_bal.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import pymysql as mysql
 import os
 
+''' temporary queries to validate against Steves balances table '''
+
 parent_dir = os.path.dirname(os.getcwd())
 dotenv_path = os.path.join(parent_dir, '.env')
 load_dotenv(dotenv_path)
@@ -30,16 +32,76 @@ mysql_conn = mysql.connect(
 # Compare id, last_update, and amt fields
 cursor = mysql_conn.cursor()
 
-# Query to get balances for the highest block_index value
+# Query to get highest last_update value from balances
 query = """
 SELECT last_update
 FROM balances
 ORDER BY last_update DESC
-LIMIT 1
 """
-
 cursor.execute(query)
 highest_block = cursor.fetchone()[0]
+print("BALANCES: Highest block in table:", highest_block)
+# highest_block = 822825
+
+# Query to get the highest block_index value from SRC_STEVE
+query = """
+select block_index
+from SRC_STEVE
+order by block_index desc
+"""
+cursor.execute(query)
+highest_block = cursor.fetchone()[0]
+print("STEVE: Highest block in table:", highest_block)
+
+# Query to get all rows in SRC_STEVE <= highest_block
+query = """
+SELECT COUNT(*)
+FROM SRC_STEVE
+WHERE block_index <= %s
+"""
+cursor.execute(query, (highest_block,))
+result = cursor.fetchone()[0]
+print(f"Count of all rows in SRC_STEVE <= block {highest_block}:", result)
+
+# Query to count all rows in balances <= highest_block
+query = """
+SELECT COUNT(*)
+FROM balances
+WHERE last_update <= %s
+"""
+cursor.execute(query, (highest_block,))
+result = cursor.fetchone()[0]
+print(f"Count of all rows in balances <= block {highest_block}:", result)
+
+
+# Query to output all rows that are in SRC_STEVE but NOT in balances where block_index is less than or equal to highest_block
+query = """
+SELECT SRC_STEVE.id, SRC_STEVE.block_index, SRC_STEVE.amt
+FROM SRC_STEVE
+LEFT JOIN balances ON SRC_STEVE.id = balances.id
+WHERE balances.id IS NULL AND SRC_STEVE.block_index <= %s
+"""
+
+cursor.execute(query, (highest_block,))
+result = cursor.fetchall()
+print(f"Count of all rows in SRC_STEVE but NOT in balances where block_index is less than or equal to {highest_block}:", len(result))
+# print each row on a separate line:
+for row in result:
+    print(" ", row)
+
+# Query to output all rows that are in balances but NOT in SRC_STEVE where last_update is less than or equal to highest_block
+query = """
+SELECT balances.id, balances.last_update, balances.amt
+FROM balances
+LEFT JOIN SRC_STEVE ON balances.id = SRC_STEVE.id
+WHERE SRC_STEVE.id IS NULL AND balances.last_update <= %s
+"""
+cursor.execute(query, (highest_block,))
+result = cursor.fetchall()
+print(f"Count of all rows in balances but NOT in SRC_STEVE where last_update is less than or equal to {highest_block}:", len(result))
+for row in result:
+    print(" ", row)
+
 
 # Query to compare id field
 query = """
@@ -51,19 +113,8 @@ WHERE balances.last_update <= %s
 
 cursor.execute(query, (highest_block,))
 result = cursor.fetchall()
-
 print("Highest Block Compared:", highest_block)
-
-# get count of total # of rows from SRC_STEVE.balances and balances
-query = """
-SELECT COUNT(*)
-FROM SRC_STEVE
-"""
-
-cursor.execute(query)
-src_steve_count = cursor.fetchone()[0]
-
-print("SRC_STEVE Row count:", src_steve_count)
+print("Count of all matched rows:", len(result))
 
 
 # Compare last_update and amt fields
@@ -85,7 +136,7 @@ for row in result:
         output.append(f"MATCH: id {id_value}, balance {balances_amt}")
     elif src_steve_block_index < balances_last_update:
         if balances_amt != src_steve_amt:
-            output.append(f"ERROR: id {id_value}, balance {balances_amt} / {src_steve_amt} BLOCKS NOT Matching S: {src_steve_block_index} / Balances {balances_last_update}")
+            output.append(f"WARN: id {id_value}, balance {balances_amt} / {src_steve_amt} BLOCKS NOT Matching S: {src_steve_block_index} / Balances {balances_last_update}")
     elif src_steve_block_index != balances_last_update:
         output.append(f"INFO: block index not matching {id_value}. Steve update: {src_steve_block_index}. Balances update: {balances_last_update}")
     else:
