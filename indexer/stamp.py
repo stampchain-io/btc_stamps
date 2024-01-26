b4be01cb3a5d5fb1727277fdac6617299d0b922a
Reinamora <reinamora@stampchain.io>
2024-01-26 17:09:30 +0000

    fix(indexer): save all file types that interpret a suffix for cursed, fix command injection risk

--- indexer/stamp.py
+++ indexer/stamp.py
@@ -11,7 +11,6 @@ import os
 import zlib
 import msgpack
 import io
-import binascii
 from datetime import datetime
 from decimal import Decimal
 
@@ -23,8 +22,6 @@ from src20 import (
     check_format,
     build_src20_svg_string,
     insert_into_src20_tables,
-    update_balances,
-    get_total_user_balance_from_db
 )
 import traceback
 from src.aws import (
@@ -327,7 +324,7 @@ def decode_base64(base64_string, block_index):
                 # this will be ok in the docker containers, but a potential problem
                 # will need to verify that there are no instances where this is su
                 command = f'printf "%s" "{base64_string}" | base64 -d 2>&1'
-                image_data = subprocess.check_output(command, shell=True)
+                image_data = subprocess.run(command, shell=True, capture_output=True, text=True, check=True, stdout=subprocess.PIPE).stdout
                 return image_data, True
             except Exception as e3:
                 # If all decoding attempts fail, print an error message and return None
@@ -717,7 +714,7 @@ def parse_tx_to_stamp_table(db, tx_hash, source, destination, btc_amount, fee, d
 
     if (
         ident in config.SUPPORTED_SUB_PROTOCOLS
-        or file_suffix in config.MIME_TYPES
+        or file_suffix # in config.MIME_TYPES
     ):
         if type(decoded_base64) is str:
             decoded_base64 = decoded_base64.encode('utf-8')
