cffbaeb7095bf9fa8c47381ec715e038d5dcff87
Reinamora <reinamora@stampchain.io>
2024-05-17 04:31:38 +0000

    chore: implement Issue #263, #262, optimize models

--- indexer/src/index_core/stamp.py
+++ indexer/src/index_core/stamp.py
@@ -1,61 +1,26 @@
 import logging
-import json
 import base64
 import pybase64
-import magic
 import subprocess  # nosec
-import zlib
-import msgpack
-import traceback
+import json
 
+from index_core.exceptions import DataConversionError, InvalidInputDataError
 from index_core.models import ValidStamp, StampData
 import index_core.log as log
-from index_core.exceptions import DataConversionError, InvalidInputDataError
 from index_core.xcprequest import parse_base64_from_description
 from index_core.database import get_next_stamp_number, check_reissue
 from index_core.util import (
-    create_base62_hash,
     check_valid_base64_string,
-    is_json_string,
     convert_to_dict_or_string
 )
 from index_core.files import store_files
-from index_core.src721 import validate_src721_and_process
-from index_core.src20 import (
-    check_format,
-    build_src20_svg_string,
-)
 from config import (
-    CP_BMN_FEAT_BLOCK_START,
     STOP_BASE64_REPAIR,
     CP_P2WSH_FEAT_BLOCK_START,
-    CP_SRC20_END_BLOCK,
-    STRIP_WHITESPACE,
-    SUPPORTED_SUB_PROTOCOLS,
-    MIME_TYPES,
-    DOMAINNAME,
-    INVALID_BTC_STAMP_SUFFIX,
 )
 
 logger = logging.getLogger(__name__)
 log.set_logger(logger)
-
-
-def get_cpid(stamp, block_index, tx_hash):
-    """
-    Get the CPID (Counterpart Identifier aka ASSET) for a given stamp.
-
-    Args:
-        stamp (dict): The stamp dictionary.
-        block_index (int): The block index.
-        tx_hash (str): The transaction hash.
-
-    Returns:
-        tuple: A tuple containing the CPID and the base62 hash.
-
-    """
-    cpid = stamp.get('cpid', None)
-    return cpid, create_base62_hash(tx_hash, str(block_index), 20)
 
 
 def decode_base64(base64_string, block_index):
@@ -133,7 +98,7 @@ def get_src_or_img_from_data(stamp, block_index):
         tuple: A tuple containing the extracted data in the following order:
             - decoded_base64 (str or None): The decoded base64 data.
             - base64_string (str or None): The original base64 string.
-            - stamp_mimetype (str or None): The MIME type of the stamp.
+            - stamp_mimetype (str or None): The MIME type of the stamp in the description.
             - is_valid_base64 (bool or None): Indicates if the base64 data is valid.
     """
     stamp_mimetype, decoded_base64, is_valid_base64 = None, None, None
@@ -155,187 +120,28 @@ def get_src_or_img_from_data(stamp, block_index):
         return decoded_base64, base64_string, stamp_mimetype, is_valid_base64
 
 
-def check_custom_suffix(bytestring_data):
-    ''' for items that aren't part of the magic module that we want to include '''
-    if bytestring_data[:3] == b'BMN':
-        return True
-    else:
-        return None
-
-
-def get_file_suffix(bytestring_data, block_index):
+def encode_and_store_file(db, tx_hash, file_suffix, decoded_base64, stamp_mimetype):
     """
-    Determines the file suffix based on the given bytestring data. The
-    block index is used to determine the consensus change when we attempted
-    repair on the base64 string for padding
-
-    Args:
-        bytestring_data (bytes): The bytestring data to analyze.
-        block_index (int): The block index.
-
-    Returns:
-        str: The file suffix.
-
-    Raises:
-        None
-
-    """
-    if block_index > CP_BMN_FEAT_BLOCK_START:
-        if check_custom_suffix(bytestring_data):
-            return 'bmn'
-    try:
-        json.loads(bytestring_data.decode('utf-8'))
-        return 'json'
-    except (json.JSONDecodeError, UnicodeDecodeError):
-        # If it failed to decode as UTF-8 text, pass it to magic to determine the file type
-        if block_index > STRIP_WHITESPACE:  # after this block we attempt to strip whitespace from the beginning of the binary data to catch Mikes A12333916315059997842
-            file_type = magic.from_buffer(bytestring_data.lstrip(), mime=True)
-        else:
-            file_type = magic.from_buffer(bytestring_data, mime=True)
-        return file_type.split('/')[-1]
-
-
-def reformat_src_string_get_ident(decoded_data):
-    """
-    Reformat the source JSON string and extract the identifier and file suffix.
-
-    This function takes a decoded data string as input and reformats it by converting all keys in the JSON object to lowercase.
-    It then checks if the reformatted data has a key 'p' (protocol) that matches one of the supported sub-protocols defined in the 'config' module.
-    If a match is found, it extracts the identifier from the 'p' key and sets the file suffix to 'json'. Otherwise, it sets the file suffix to None and the identifier to 'UNKNOWN'.
-
-    Args:
-        decoded_data (str): The decoded data string.
-
-    Returns:
-        tuple: A tuple containing the identifier and file suffix.
-    """
-    if not isinstance(decoded_data, dict):
-        decoded_data = json.loads(decoded_data)
-    decoded_data = {k.lower(): v for k, v in decoded_data.items()}
-    if decoded_data and decoded_data.get('p') and decoded_data.get('p').upper() in SUPPORTED_SUB_PROTOCOLS:
-        ident = decoded_data['p'].upper()
-        file_suffix = 'json'
-    else:
-        file_suffix = None
-        ident = 'UNKNOWN'
-    return ident, file_suffix
-
-
-def zlib_decompress(compressed_data, block_index):
-    """
-    Decompresses zlib-compressed data and returns the decompressed data as a JSON string.
-
-    Args:
-        compressed_data (bytes): The zlib-compressed data to decompress.
-
-    Returns:
-        tuple: A tuple containing the identifier, file suffix, and JSON string of the decompressed data.
-            - identifier (str): The identifier of the decompressed data.
-            - file_suffix (str): The file suffix indicating the format of the decompressed data.
-            - json_string (str): The decompressed data as a JSON string.
-
-    Raises:
-        zlib.error: If there is an error decompressing the zlib data.
-        msgpack.exceptions.ExtraData: If there is an error decoding the MessagePack data.
-        TypeError: If the decoded data is not JSON-compatible.
-    """
-    try:
-        uncompressed_data = zlib.decompress(compressed_data)  # suffix = plain /  Uncompressed data: b'\x85\xa1p\xa6src-20\xa2op\xa6deploy\xa4tick\xa4ordi\xa3max\xa821000000\xa3lim\xa41000'
-        # DEBUG: msgpack support for all stamps
-        # uncompressed_file_suffix = get_file_suffix(uncompressed_data, block_index)
-        # if uncompressed_file_suffix == 'plain':
-        #     print("found plaintext - check for json string")
-        #     # may need to do msgpack here.
-        #     if (type(uncompressed_data) is str and is_json_string(uncompressed_data)):
-        #         print("found json string")
-
-        decoded_data = msgpack.unpackb(uncompressed_data)  # {'p': 'src-20', 'op': 'deploy', 'tick': 'kevin', 'max': '21000000', 'lim': '1000'}
-        json_string = json.dumps(decoded_data)
-        file_suffix = "json"
-        ident, file_suffix = reformat_src_string_get_ident(json_string)
-        return ident, file_suffix, json_string
-    except zlib.error:
-        logger.info("EXCLUSION: Error decompressing zlib data")
-        return 'UNKNOWN', 'zlib', compressed_data
-    except msgpack.exceptions.ExtraData:
-        logger.info("EXCLUSION: Error decoding MessagePack data")
-        return 'UNKNOWN', 'zlib', compressed_data
-    except TypeError:
-        logger.info("EXCLUSION: The decoded data is not JSON-compatible")
-        return 'UNKNOWN', 'zlib', compressed_data
-
-
-def check_decoded_data_fetch_ident(decoded_data, block_index, ident):
-    '''
-    Check the decoded data and fetch the identifier and file suffix.
-
-    Parameters:
-        decoded_data (bytes or dict or str): The decoded data, which can be a bytes object, a dictionary, or a string.
-        block_index (int): The block index.
-        ident (str): The identifier.
-
-    Returns:
-        tuple: A tuple containing the identifier(STAMP, SRC-20/721), file suffix, and the decoded base64 data.
-        If decoded base64 is a string it returns a dict
-
-    Raises:
-        Exception: If an error occurs during the process.
-
-    '''
-
-    file_suffix = None
-    if type(decoded_data) is bytes:
-        try:
-            decoded_data = decoded_data.decode('utf-8')
-        except Exception as e:
-            logger.warning(f"Error decoding bytes: {e}")
-            pass
-    if (type(decoded_data) is dict):
-        ident, file_suffix = reformat_src_string_get_ident(decoded_data)
-    elif (type(decoded_data) is str and is_json_string(decoded_data)):
-        ident, file_suffix = reformat_src_string_get_ident(decoded_data)
-    else:
-        try:
-            if decoded_data and type(decoded_data) is str:
-                decoded_data_bytestring = decoded_data.encode('utf-8')
-                file_suffix = get_file_suffix(decoded_data_bytestring, block_index)
-                ident = 'STAMP'
-            elif decoded_data and type(decoded_data) is bytes:
-                file_suffix = get_file_suffix(decoded_data, block_index)
-                if file_suffix in ['zlib']:
-                    ident, file_suffix, decoded_data = zlib_decompress(decoded_data, block_index)
-                else:
-                    ident = 'STAMP'
-            else:
-                file_suffix = None
-                ident = 'UNKNOWN'
-        except Exception as e:
-            logger.error(f"Error: {e}\n{traceback.format_exc()}")
-            raise
-    return ident, file_suffix, decoded_data
-
-
-def encode_and_store_file(db, tx_hash, file_suffix, decoded_base64, stamp_mimetype, SUPPORTED_SUB_PROTOCOLS, ident):
-    """
-    Encodes the decoded_base64 string to utf-8 (if it's a string), constructs the filename,
+    Encodes the decoded_base64 string to utf-8 (if it's a string or a dict), constructs the filename,
     and stores the file.
 
     Args:
         db: The database connection object.
         tx_hash (str): The transaction hash.
         file_suffix (str): The file suffix.
-        decoded_base64 (bytes or str): The decoded base64 data.
+        decoded_base64 (bytes, str, or dict): The decoded base64 data.
         stamp_mimetype (str): The MIME type of the stamp.
-        SUPPORTED_SUB_PROTOCOLS (list): A list of supported sub-protocols.
-        ident (str): The identifier indicating the type of stamp.
 
     Returns:
         The result of the file storage operation.
     """
-    if ident in SUPPORTED_SUB_PROTOCOLS or file_suffix:
+    if file_suffix:
+        if isinstance(decoded_base64, dict):
+            decoded_base64 = json.dumps(decoded_base64)
         if isinstance(decoded_base64, str):
             decoded_base64 = decoded_base64.encode('utf-8')
         filename = f"{tx_hash}.{file_suffix}"
+        logger.info(decoded_base64)
         return store_files(db, filename, decoded_base64, stamp_mimetype)
     return None, None
 
@@ -398,51 +204,14 @@ def parse_stamp(*, stamp_data: StampData, db, valid_stamps_in_block: list[ValidS
         Exception: If an unexpected condition occurs during stamp processing.
 
     """
-    filename = stamp_results = src20_dict = prevalidated_src20 = None
+    stamp_results = src20_dict = prevalidated_src20 = None
     valid_stamp: ValidStamp = {}
-
     try:
-        stamp_data.validate_data()
-        stamp = convert_to_dict_or_string(stamp_data.data, output_format='dict')
+        stamp_data.process_and_store_stamp_data(
+            get_src_or_img_from_data, convert_to_dict_or_string, encode_and_store_file, check_reissue, decode_base64, db, valid_stamps_in_block)
     except (DataConversionError, InvalidInputDataError, ValueError) as e:
-        print(f"Invalid Stamp Data {e}")
+        logger.warning(f"Invalid Stamp Data: {e}")
         return (None,) * 4
-
-    stamp_data.get_src_or_img(get_src_or_img_from_data, stamp)
-    stamp_data.update_cpid_and_stamp_hash(get_cpid, stamp)
-    stamp_data.update_stamp_data_rows(stamp)
-
-    if stamp_data.is_reissue(check_reissue, db, valid_stamps_in_block):
-        return (None,) * 4
-
-    stamp_data.process_stamp_data(decode_base64, check_decoded_data_fetch_ident, CP_P2WSH_FEAT_BLOCK_START)
-    stamp_data.normalize_file_suffix()
-
-    if stamp_data.valid_src20(CP_SRC20_END_BLOCK):
-        src20_dict = check_format(stamp_data.decoded_base64, stamp_data.tx_hash)
-        if src20_dict is not None:
-            stamp_data.is_btc_stamp = 1
-            stamp_data.decoded_base64 = build_src20_svg_string(db, src20_dict)
-            stamp_data.file_suffix = 'svg'
-        else:
-            return (None,) * 4
-
-    if stamp_data.valid_src721(CP_P2WSH_FEAT_BLOCK_START):
-        stamp_data.src_data = stamp_data.decoded_base64
-        stamp_data.is_btc_stamp = 1
-        svg_output, stamp_data.file_suffix = validate_src721_and_process(stamp_data.src_data, valid_stamps_in_block, db)
-        stamp_data.decoded_base64 = svg_output
-        stamp_data.file_suffix = 'svg'
-
-    if (stamp_data.ident != 'UNKNOWN' and stamp_data.asset_longname is None and stamp_data.cpid and stamp_data.cpid.startswith('A') and not stamp_data.is_op_return and stamp_data.file_suffix not in INVALID_BTC_STAMP_SUFFIX):
-        stamp_data.is_btc_stamp = 1
-    elif stamp_data.asset_longname is not None:
-        stamp_data.cpid = stamp_data.asset_longname
-        stamp_data.is_cursed = 1
-        stamp_data.is_btc_stamp = None
-    elif (stamp_data.cpid and (stamp_data.file_suffix in INVALID_BTC_STAMP_SUFFIX or not stamp_data.cpid.startswith('A') or stamp_data.is_op_return)):
-        stamp_data.is_btc_stamp = None
-        stamp_data.is_cursed = 1
 
     if stamp_data.is_btc_stamp:
         stamp_data.stamp = get_next_stamp_number(db, 'stamp')
@@ -455,16 +224,9 @@ def parse_stamp(*, stamp_data: StampData, db, valid_stamps_in_block: list[ValidS
         valid_stamp = create_valid_stamp_dict(
             stamp_data.stamp, stamp_data.tx_hash, stamp_data.cpid, stamp_data.is_btc_stamp, stamp_data.is_valid_base64, stamp_data.stamp_base64, stamp_data.is_cursed, stamp_data.src_data)
 
-    if stamp_data.valid_src20(CP_SRC20_END_BLOCK):
+    if stamp_data.pval_src20:
+        src20_dict = stamp_data.src20_dict
         prevalidated_src20 = append_stamp_data_to_src20_dict(stamp_data, src20_dict)
 
-    stamp_data.update_mime_type(MIME_TYPES)
-
-    stamp_data.file_hash, filename = encode_and_store_file(
-        db, stamp_data.tx_hash, stamp_data.file_suffix, stamp_data.decoded_base64, stamp_data.stamp_mimetype, SUPPORTED_SUB_PROTOCOLS, stamp_data.ident)
-
-    stamp_data.update_cpid_and_stamp_url(DOMAINNAME, filename)
     stamp_results = True
-    # NOTE: parsed_stamp includes cursed stamps and non-numbered stamps
-    # valid_stamp is is_btc_stamp only
     return stamp_results, stamp_data, valid_stamp, prevalidated_src20
