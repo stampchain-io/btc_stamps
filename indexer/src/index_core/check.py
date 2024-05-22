534c5e82cf765908fe9a9387ef54790e4dd5f028
reinamora <reinamora@stampchain.io>
2024-05-22 04:17:04 +0000

    chore: update checkpoints for release

--- indexer/src/index_core/check.py
+++ indexer/src/index_core/check.py
@@ -19,6 +19,24 @@ CHECKPOINTS_MAINNET = {
     781100: {'ledger_hash': '', 'txlist_hash': 'facb1127746e4d82fbf47706c3fb4a4f38314251452991d5ba9be008d22812fd'},
     781300: {'ledger_hash': '', 'txlist_hash': 'dd70a1179c22ed68d15f906cf3fc2f1f72a0250d7bfcdfd00ba04e1339189756'},
     782285: {'ledger_hash': '', 'txlist_hash': 'f712708dd191802e9e1903cbad19ca58fc159fbec4eeb2026bfed917815ff38d'},  # first svg stamp
+    785000: {'ledger_hash': '', 'txlist_hash': '96f626e23f8f10b9349dba8250db9c7c48d1e5bad8e5997f8762ac2ead9586ab'},
+    788041: {'ledger_hash': 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', 'txlist_hash': '45589cad7fdb28b6ddeea9aa0ee297a58b0f3208e46f81321626a6e4c6826013'},
+
+    788127: {'ledger_hash': '32fac3ac1708f5ad22ee9537752e187307b692b069d68dc8fcaf93210421cd16', 'txlist_hash': '6e10dbd660ed929dc32e430d010e300d39c92018d99f63d1d5e83a52be327410'},
+    788134: {'ledger_hash': '1d9fd11b3fb6b96a1a9ae676bf84d674316f05dbec2d489709ebbbf48c1a0d84', 'txlist_hash': '93a3dbef0ec5f194c461f0c9448269d670b5063983842c1dff5cb3bb73a5d0ef'},
+
+    790000: {'ledger_hash': '', 'txlist_hash': '68d522a0eeebbe4fde146191613d18f5b26ca8372fe639ac4da0ceb3ab746ad5'},
+    795000: {'ledger_hash': '', 'txlist_hash': '8d28ff2c1ba020bada48c6b9fe191b64f0442f5dc4b1d968b5656bd640868e00'},
+    800000: {'ledger_hash': '', 'txlist_hash': 'ac420148167f197e17841387b81685162c0dda13dcc5671867af842a4a466540'},
+    805000: {'ledger_hash': '', 'txlist_hash': 'b673db6ec0f2790b24b896248a54eaefcd47b970a474806f5f340d12ae095650'},
+    810000: {'ledger_hash': '', 'txlist_hash': '41ab342679fac87a4f35a8ecf43b575ad700b65b98817d9acf3d710bf19a9102'},
+    815000: {'ledger_hash': '', 'txlist_hash': '9fdef899ae689dc8dcd567fbf642dbced3759c8596e2639c19a27c7e23b87d08'},
+    820000: {'ledger_hash': 'aee64e20dc83d09852c78c3caa9c39a61e77c0fa946843f39838963046f15932', 'txlist_hash': '1570e6066de3a38161b583533d59d37f42e9b758a3799bbda6947b10332ef1fd'},
+    825000: {'ledger_hash': '68e0621468999995719b13a974ed53047c88b63bb4c211f30c63d7afd0a52159', 'txlist_hash': '7dbe4be3a8bff790618a4453f3a10ab0d0f9f865d40a5b0df80ef5a7d745d368'},
+    830000: {'ledger_hash': 'e3bb684fba10cedf0dc5778a85aab66a42e500fad5405fe245a5bf80f313523b', 'txlist_hash': 'dfb1ce285bae78e5910569a3eff13e6476e7f0ba33002540e76d458944e00e0a'},
+    835000: {'ledger_hash': '78e5d16e8802ea0d751bee5d9df2411b71f64032ae5ad7b98aec1617ecb082a3', 'txlist_hash': '152292be3ef84e2aa636ad3476db1c6a345c47179cf9c84734cd15a1e8280a0d'},
+    840000: {'ledger_hash': '', 'txlist_hash': '971ea871bc0c5a6a2c89fe74d038cf1957d2d655328efd65e53101e4e256b868'},
+
 }
 
 CONSENSUS_HASH_VERSION_TESTNET = 7
