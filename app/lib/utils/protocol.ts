c78aa2fda762c72a8cd91b1c4ef3d39892ba461b
reinamora <reinamora@stampchain.io>
2024-06-14 16:38:49 +0000

    chore: remove app, fix docker

--- app/lib/utils/protocol.ts
+++
@@ -1,1 +0,0 @@
-export const PROTOCOL_IDENTIFIERS = ['STAMP', 'SRC-20', 'SRC-721'];
--- app/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-export function handler(req: Request): Response {
-  return new Response("", {
-    status: 307,
-    headers: { Location: "/block/last" },
-  });
-}
