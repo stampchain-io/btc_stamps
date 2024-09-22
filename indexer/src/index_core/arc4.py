4944350c40367bddfd26eab47286b95bf7ce9873
Kevin <reinamora@stampchain.io>
2024-09-22 13:01:46 -0500

    chore: update imports for cryptography module changes and run tests on arc4 (#413)
    crypto module moved the location of arc4 to decrepit :)

--- indexer/src/index_core/arc4.py
+++ indexer/src/index_core/arc4.py
@@ -1,14 +1,15 @@
 import binascii
 
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
+from cryptography.hazmat.decrepit.ciphers.algorithms import ARC4  # Updated import path
+from cryptography.hazmat.primitives.ciphers import Cipher
 
 
 def init_arc4(seed):
     if isinstance(seed, str):
         seed = binascii.unhexlify(seed)
     backend = default_backend()
-    cipher = Cipher(algorithms.ARC4(seed), mode=None, backend=backend)  # nosec
+    cipher = Cipher(ARC4(seed), mode=None, backend=backend)  # nosec
     return cipher
 
 
@@ -16,3 +17,7 @@ def arc4_decrypt_chunk(cyphertext, key):
     """Un-obfuscate. initialize key once per attempt."""
     decryptor = key.decryptor()
     return decryptor.update(cyphertext) + decryptor.finalize()
+
+
+def get_arc4_path():
+    return ARC4.__module__
