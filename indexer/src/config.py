a44bc12bdc74fb8401d9c1a01b3b603b917ecf9a
reinamora <reinamora@stampchain.io>
2024-11-25 14:38:41 +0000

    chore: update dev canary versioning [skip-version]

--- indexer/src/config.py
+++ indexer/src/config.py
@@ -234,7 +234,7 @@ TICK_PATTERN_SET = UNICODE_SET.union(CHAR_SET)
 
 
 # Versions
-VERSION_STRING = "1.8.26"
+VERSION_STRING = "1.8.26+canary.0"
 
 
 def update_version_globals(version_string: str):
