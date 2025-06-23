b377c1c78fe04981ac1f2209fc85bee3afe5d18d
GitHub Action <action@github.com>
2025-06-23 19:50:20 +0000

    Bump version: 1.8.26+canary.182 → 1.8.26+canary.183

--- indexer/src/config.py
+++ indexer/src/config.py
@@ -516,7 +516,7 @@ TICK_PATTERN_SET = UNICODE_SET.union(CHAR_SET)
 
 
 # Versions
-VERSION_STRING = "1.8.26+canary.182"
+VERSION_STRING = "1.8.26+canary.183"
 
 
 def update_version_globals(version_string: str):
